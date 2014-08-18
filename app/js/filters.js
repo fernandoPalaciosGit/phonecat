4d1d6aa007a7404812a4219d3d079d7527c3ba1e
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-08-18 17:44:22 +0200

    filtros personalizados

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,15 @@
 
 /* Filters */
 
+// mostrar la version de angular
 angular.module('phoneApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }]);
+	filter('interpolate', ['version', function(version) {
+		return function (text) {
+			return String(text).replace(/\%VERSION\%/mg, version);
+		};
+	}]).
+	filter('checkmark', function(){
+		return function (input){
+			return ( !!input ) ? '\u2713' : '\u2718';
+		};
+	});
